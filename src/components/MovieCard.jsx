c75a26cd5b704b8ba038bd15d824373c59142991
Ye Min Aung <yeminaung013@gmail.com>
2023-11-07 18:03:32 +0630

    change styling in movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ const MovieCard = ({ Year, Poster, Title, Type }) => {
 	return (
 		<div className="movie group relative w-80 h-[460px] shadow-sm shadow-[#f9d3b4] rounded-xl overflow-hidden transition-transform hover:scale-105">
 			<div className="absolute p-[16px] group-hover:opacity-100 w-full opacity-0 top-0 text-[#f9d3b4]">
-				<p className="text-[#f9d3b4]">{Year}</p>
+				<p>{Year}</p>
 			</div>
 			<div
 				className={`w-full h-full group-hover:${isDark ? "opacity-30" : "opacity-50"}`}
@@ -19,9 +19,8 @@ const MovieCard = ({ Year, Poster, Title, Type }) => {
 				/>
 			</div>
 			<div
-				className={`group-hover:${isDark ? "bg-transparent" : "bg-[#343739]"} ${
-					isDark ? "bg-[#343739]" : "bg-transparent"
-				} absolute bottom-0 right-0 left-0 z-10 pt-4 px-6 pb-6`}
+				className="group-hover:bg-transparent absolute bottom-0 right-0 left-0 z-10 pt-4 px-6 pb-6"
+				style={{ backgroundColor: "#343739" }}
 			>
 				<span className="uppercase text-sm font-semibold text-white">{Type}</span>
 				<h3 className="mt-5px text-[#f9d3b4]">{Title}</h3>
